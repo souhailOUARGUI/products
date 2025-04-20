020017b56437601dce694b4eb235edd08928543c
souhailOUARGUI <ouargui550@gmail.com>
2025-04-20 11:31:39 +0100

    created Product class, added application.properties

--- src/main/java/ma/enset/products/entities/product.java
+++ src/main/java/ma/enset/products/entities/product.java
@@ -1,9 +1,17 @@
 package ma.enset.products.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private double price;
