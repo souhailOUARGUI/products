57ea1ff8977f6624af57042c3acbe3a391a29db5
souhailOUARGUI <ouargui550@gmail.com>
2025-04-20 11:52:11 +0100

    created products with productRepository on ProductsApplication

--- src/main/java/ma/enset/products/ProductsApplication.java
+++ src/main/java/ma/enset/products/ProductsApplication.java
@@ -1,18 +1,26 @@
 package ma.enset.products;
 
+import ma.enset.products.entities.product;
+import ma.enset.products.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductsApplication implements CommandLineRunner {
-
+    @Autowired
+    private ProductRepository productRepository;
     public static void main(String[] args) {
         SpringApplication.run(ProductsApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+    productRepository.save(new product(null, "Computer", 4300, 3));
+    productRepository.save(new product(null, "Phone", 1200, 10));
+    productRepository.save(new product(null, "Tablet", 2500, 5));
+
 
     }
 }
